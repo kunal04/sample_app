72de0955109f6b2a0ca880379846153b51280a18
kunal kapadia <kapadikuna@gmail.com>
2013-06-15 18:12:18 -0700

    Finish updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
 
-
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
   	@user = User.find(params[:id])
   end
@@ -20,4 +26,44 @@ class UsersController < ApplicationController
   	end	
   end
 
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "user has been deleted"
+    redirect_to users_path
+  end
+
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+          flash[:notive] = "Please sign in"
+          redirect_to signin_path
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_path unless current_user.admin?
+    end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,13 @@
 FactoryGirl.define do 
 	factory :user do 
-		name "Kunal Kapadia"
-		email "kapadikuna@example.com"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "Person_#{n}@example.com" }
 		password "foobar"
 		password_confirmation "foobar"
+
+		factory :admin do 
+			admin true
+		end
+
 	end
-	
 end
